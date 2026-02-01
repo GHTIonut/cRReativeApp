b0d9e450728c9d08042658fa7bbf9775e0685081
GHTIonut <ghitaionutcristian96@yahoo.com>
2026-02-01 02:03:36 +0200

    Subscription form base created. Subscription form css added.

--- cRReativeApp/src/profilePages/Subscription.jsx
+++ cRReativeApp/src/profilePages/Subscription.jsx
@@ -1,4 +1,5 @@
 import ProfileMenu from "../Components/ProfileMenu";
+import "../styles/subscription.css";
 
 export default function HoroscopeSubscription() {
   return (
@@ -6,6 +7,29 @@ export default function HoroscopeSubscription() {
       <div className="profileMenu">
         <ProfileMenu />
       </div>
+      <main>
+        <section>
+          <form className="subscriptionForm">
+            <h1>Subscription form</h1>
+            <div>
+              <label htmlFor="cardHolder">Card holder:</label>
+              <input type="text" name="cardHolder" id="cardHolder" />
+              <label htmlFor="cardNumber">Card number:</label>
+              <input
+                type="text"
+                name="cardNumber"
+                id="cardNumber"
+                pattern="^[0-9]{16}$"
+              />
+              <label htmlFor="expiryDate">Expiry date:</label>
+              <input type="text" name="expiryDate" id="expiryDate" />
+              <label htmlFor="cvv">CVV:</label>
+              <input type="text" name="cvv" id="cvv" />
+            </div>
+            <button>Pay</button>
+          </form>
+        </section>
+      </main>
     </>
   );
 }
