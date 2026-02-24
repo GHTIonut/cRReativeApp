a943e60103bba6c23fd1dfc6eff20f63b3f2bdcc
GHTIonut <ghitaionutcristian96@yahoo.com>
2026-02-24 14:29:19 +0200

    To do list function step 1.

--- cRReativeApp/src/profilePages/ToDoList.jsx
+++ cRReativeApp/src/profilePages/ToDoList.jsx
@@ -5,31 +5,54 @@ import "../styles/toDoContainer.css";
 export function ToDoList() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newToDo = { title: title, description: description };
+    setToDoList([...toDoList, newToDo]);
+    setTitle("");
+    setDescription("");
+  }
+
+  function deleteToDo(indexToDelete) {
+    const updatedList = toDoList.filter((_, index) => index !== indexToDelete);
+    setToDoList(updatedList);
+  }
 
   return (
     <>
-      <ProfileMenu />
-      <div className="toDoContainer">
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          id="title"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-
-        <label htmlFor="description">Description</label>
-        <input
-          type="text"
-          id="description"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <button>Save</button>
-      </div>
-      <div>
-        <div>{title}</div>
-        <div>{description}</div>
-      </div>
+      <section className="toDoFormContainer">
+        <form className="toDoForm">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button type="button" onClick={handleSubmit}>
+            Save
+          </button>
+          <div>
+            {toDoList.map((toDo, index) => (
+              <div key={index}>
+                <h2>{toDo.title}</h2> <p>{toDo.description}</p>{" "}
+                <button type="button" onClick={() => deleteToDo(index)}>
+                  Delete
+                </button>
+              </div>
+            ))}
+          </div>
+        </form>
+      </section>
     </>
   );
 }
